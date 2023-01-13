b3ae6a468bb041a2f7db413db30f885a8fa4dfe7
Orxan7 <orhan2@bk.ru>
2023-01-13 22:24:13 +0400

    Last Commit 6

--- src/api/fetchTrackApi.js
+++ src/api/fetchTrackApi.js
@@ -1,8 +1,6 @@
 export async function fetchTrackFromApi ({fst_x, fst_y, snd_x, snd_y}) {
-    const url = `http://router.project-osrm.org/route/v1/driving/${fst_y},${fst_x};${snd_y},${snd_x}?overview=full&geometries=geojson`
-    let res = await fetch(url,{
-        "Access-Control-Allow-Origin": '*'
-    })
+    const url = `https://router.project-osrm.org/route/v1/driving/${fst_y},${fst_x};${snd_y},${snd_x}?overview=full&geometries=geojson`
+    let res = await fetch(url)
     let data = await res.json()
     return data
 }
